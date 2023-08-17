0600de765959dba38398a0993c78d101e83beaaf
mykelismyname <michealabaho265@gmail.com>
2023-08-17 20:15:26 +0100

    computing mask specifici weight matrix using both train and evaluation data

--- mslm/loss.py
+++ mslm/loss.py
@@ -6,50 +6,43 @@ class Loss(ABC):
         pass
 
 class MslmLoss(Loss):
-    def __init__(self, dataset, batch_size, seq_len, *args, **kwargs):
+    def __init__(self, batch_size, seq_len, *args, **kwargs):
         self.batch_size = batch_size
         self.seq_len = seq_len
         self._criterion = torch.nn.CrossEntropyLoss(reduction='none')
         self._softmax = torch.nn.Softmax(dim=0)
-        tracked_dataset_ids = self.identify_tokens_with_and_without_masks(dataset)
-        self.entity_mask = tracked_dataset_ids[0]
-        self.non_entity_mask = tracked_dataset_ids[1]
-        self.non_masked = tracked_dataset_ids[2]
-        self.computed_weights = self.compute_weights(self.entity_mask,
-                                                     self.non_entity_mask,
-                                                     self.non_masked)
-        self.weights = self.compute_mask_specific_weights()
-
 
     #retrieve tensors with ids of entity-level, base-level masks as well as non-masked tokens
-    def identify_tokens_with_and_without_masks(self, dataset):
-        entity_mask = []
-        non_entity_mask = []
-        non_masked = []
-
-        for d in dataset:
-            batch_size = d['input_ids'].size(0)
-            d_entity_mask, d_non_entity_mask, d_non_masked = [], [], []
-            for k in range(batch_size):
-                entity_mask_no = d['entity_specific_mask_ids'][k]
-                non_entity_mask_no = d['non_entity_specific_mask_ids'][k]
-                entity_mask_no = entity_mask_no > 0
-                entity_mask_no_list = torch.flatten(entity_mask_no.nonzero()).tolist()
-                non_entity_mask_no = non_entity_mask_no > 0
-                non_entity_mask_no_list = torch.flatten(non_entity_mask_no.nonzero()).tolist()
-                non_mask_list = [i for i in range(len(entity_mask_no)) if
-                                 i not in entity_mask_no_list + non_entity_mask_no_list]
-                d_entity_mask.append(entity_mask_no_list)
-                d_non_entity_mask.append(non_entity_mask_no_list)
-                d_non_masked.append(non_mask_list)
-            entity_mask.append(d_entity_mask)
-            non_entity_mask.append(d_non_entity_mask)
-            non_masked.append(d_non_masked)
-        return entity_mask, non_entity_mask, non_masked
+    def identify_tokens_with_and_without_masks(self, datasets):
+        datasets_masked_ids = []
+        for dataset in datasets:
+            entity_mask = []
+            non_entity_mask = []
+            non_masked = []
+            for d in dataset:
+                batch_size = d['input_ids'].size(0)
+                d_entity_mask, d_non_entity_mask, d_non_masked = [], [], []
+                for k in range(batch_size):
+                    entity_mask_no = d['entity_specific_mask_ids'][k]
+                    non_entity_mask_no = d['non_entity_specific_mask_ids'][k]
+                    entity_mask_no = entity_mask_no > 0
+                    entity_mask_no_list = torch.flatten(entity_mask_no.nonzero()).tolist()
+                    non_entity_mask_no = non_entity_mask_no > 0
+                    non_entity_mask_no_list = torch.flatten(non_entity_mask_no.nonzero()).tolist()
+                    non_mask_list = [i for i in range(len(entity_mask_no)) if
+                                     i not in entity_mask_no_list + non_entity_mask_no_list]
+                    d_entity_mask.append(entity_mask_no_list)
+                    d_non_entity_mask.append(non_entity_mask_no_list)
+                    d_non_masked.append(non_mask_list)
+                entity_mask.append(d_entity_mask)
+                non_entity_mask.append(d_non_entity_mask)
+                non_masked.append(d_non_masked)
+            datasets_masked_ids.append([entity_mask, non_entity_mask, non_masked])
+        return datasets_masked_ids
 
     #calculate a weight for the arbitrary masked tokens (base level masking), entity masked tokens
     # (entity-level masking) as well as non-masked tokens
-    def compute_weights(self, e_ms, ne_ms, n_ms):
+    def compute_weights(self, train_ids, eval_ids):
         def flatten_batch_of_lists(x):
             output = []
             for batch in x:
@@ -58,9 +51,16 @@ class MslmLoss(Loss):
                         output.append(index)
             return output
 
-        len_e_ms = len(flatten_batch_of_lists(e_ms))
-        len_ne_ms = len(flatten_batch_of_lists(ne_ms))
-        len_n_ms = len(flatten_batch_of_lists(n_ms))
+        len_e_ms, len_ne_ms, len_n_ms = 0, 0, 0 #number of entity tokens masked, arbitrary masked tokens and non masked tokens
+        for d in [train_ids, eval_ids]:
+            e, f, g = d
+            len_e_ms += len(flatten_batch_of_lists(e))
+            len_ne_ms += len(flatten_batch_of_lists(f))
+            len_n_ms += len(flatten_batch_of_lists(g))
+
+        print(f"Number of entity tokens masked: {len_e_ms}")
+        print(f"Number of arbtrary masked tokens: {len_ne_ms}")
+        print(f"Number of non masked tokens: {len_n_ms}")
         total_len = len_e_ms + len_ne_ms + len_n_ms
 
         self.weight_matrix = torch.zeros(3)
@@ -69,15 +69,16 @@ class MslmLoss(Loss):
         return self._softmax(self.weight_matrix)
 
     #compute a tensor with a weight for each token with respect to the mask (mask specific weight)
-    def compute_mask_specific_weights(self):
-        assert len(self.entity_mask) == len(self.non_entity_mask) == len(self.non_masked), "Something is wrong"
-        n = len(self.entity_mask)
+    def compute_mask_specific_weights(self, data, weight_matrix):
+        entity_masked_data, non_entity_masked_data, non_masked_data = data
+        assert len(entity_masked_data) == len(non_entity_masked_data) == len(non_masked_data), "Something is wrong"
+        n = len(entity_masked_data)
         weights = torch.zeros(n, self.batch_size, self.seq_len)
         for b in range(n):
-            for i in range(len(self.entity_mask[b])):
-                weights[b][i, self.entity_mask[b][i]] = self.computed_weights[0]
-                weights[b][i, self.non_entity_mask[b][i]] = self.computed_weights[1]
-                weights[b][i, self.non_masked[b][i]] = self.computed_weights[2]
+            for i in range(len(entity_masked_data[b])):
+                weights[b][i, entity_masked_data[b][i]] = weight_matrix[0]
+                weights[b][i, non_entity_masked_data[b][i]] = weight_matrix[1]
+                weights[b][i, non_masked_data[b][i]] = 1
         return weights
 
     #compute a cross entropy loss
