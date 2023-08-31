f361f4e1e19690126d4267ac83638fe9271a976a
mykelismyname <michealabaho265@gmail.com>
2023-08-31 17:41:15 +0100

    replacing the original input ids with masked input ids

--- mslm/models/mslm_model.py
+++ mslm/models/mslm_model.py
@@ -7,6 +7,10 @@ class MslmModel(nn.Module):
         self.model = model
 
     def forward(self, batch_input):
+        batch_input = {k: v for k, v in batch_input.items() if k not in ['entity_specific_mask_ids',
+                                                                         'non_entity_specific_mask_ids',
+                                                                         'input_ids']}
+        batch_input['input_ids'] = batch_input.pop('masked_input_ids')
         output = self.model(**batch_input, output_hidden_states=True)
         return output
 
