4588d0900f2f93c6ec989a99d22a65f0a0793bb2
mykelismyname <michealabaho265@gmail.com>
2023-08-31 17:42:01 +0100

    adding losses from the mask languge mode and the detection model

--- mslm/models/joint_model.py
+++ mslm/models/joint_model.py
@@ -3,6 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from mslm.models.detection import DetectionModel
 from mslm.models.mslm_model import MslmModel
+from mslm.loss import MslmLoss, DetectionLoss
+from typing import Optional, Tuple
+
+class Output:
+    loss: Optional[torch.FloatTensor] = None
+    logits: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
 
 class BasicModule(torch.nn.Module):
     def __init__(self):
@@ -19,17 +26,31 @@ class BasicModule(torch.nn.Module):
         return path
 
 class JointModel(BasicModule):
-    def __init__(self, model, config, token_types, drop_out, meta_embedding_dim):
+    def __init__(self, model, config, token_types, drop_out, reduction, batch_size, max_length, meta_embedding_dim):
         super().__init__()
+        self.config = config
         self.model = model
         self.mslm_model = MslmModel(self.model)
         self.detection = DetectionModel(config=config,
                                         token_types=token_types,
                                         drop_out=drop_out,
                                         meta_embedding_dim=meta_embedding_dim)
+        self.mslm_loss = MslmLoss(batch_size, max_length, config.vocab_size)
+        self.det_loss = DetectionLoss(len(token_types))
+        self.reduction = reduction
+        self.output = Output()
 
-    def forward(self, input):
+    def forward(self, input, labels, ms_weights, weight_matrix):
         mslm_output = self.mslm_model(input)
+        mslm_labels = input['input_ids']
+        input_size = mslm_labels.size(0)
+        ms_weights = ms_weights[:input_size]
+        mslm_loss = self.mslm_loss.compute_loss(mslm_output, mslm_labels, ms_weights, weight_matrix, reduction=self.reduction)
         mslm_hidden_states = mslm_output.get("hidden_states")
         det_output = self.detection(input, mslm_hidden_states)
-        return mslm_output, det_output
+        det_loss = self.det_loss.compute_loss(det_output, labels, ms_weights, weight_matrix, reduction=self.reduction)
+        loss = mslm_loss + det_loss
+        self.output.loss = loss
+        self.output.logits = det_output
+        self.output.hidden_states = mslm_hidden_states
+        return self.output
