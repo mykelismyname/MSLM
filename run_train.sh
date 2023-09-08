377347a0b3960442f727f1c6ff2bbbfdda6a54be
mykelismyname <michealabaho265@gmail.com>
2023-09-08 18:15:54 +0100

    process other command line arguments for the dataset to fine-tune on

--- run_train.sh
+++ run_train.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
-export SAVE_DIR=output
+export SAVE_DIR="output"
 #export DATASET_NAME="BioRED-Chem"
-export DATA_DIR=../WeLT/datasets/NER
 export PRE_TRAINED_MODEL=dmis-lab/biobert-v1.1
 export BATCH_SIZE=8
 export LOSS_REDUCTION='mean'
 export MLM_PROB=0
-export EPOCHS=20
+export EPOCHS=3
 export MAX_LENGTH=256
 export META_EMBEDDING_SIZE=50
 
-datasets=("linnaeus")
+ner_datasets=("BC2GM" "BC5CDR-chem" "BC5CDR-disease" "BioRED-Chem" "JNLPBA" "linnaeus" "NCBI-disease")
 
-dmeta_emb_sizes=(50 100 150 200)
+if echo "${ner_datasets[@]}" | grep -qw "$1"; then
+  DATA_LOADING_SCRIPT=mslm/ner_dataload.py
+  DATA_DIR=../WeLT/datasets/NER/
+  DEV_SET="devel.txt"
+  datasets=($1)
+elif [ "$1" = "ebm-comet" ]; then
+  DATA_LOADING_SCRIPT=mslm/ebm_comet_dataload.py
+  DATA_DIR=../outcome_generation/data/
+  DEV_SET="dev.txt"
+  datasets=($1)
+elif [ "$1" = "ebm-nlp" ]; then
+  DATA_LOADING_SCRIPT=mslm/ebm_comet_dataload.py
+  DATA_DIR=../outcome_generation/data/
+  DEV_SET="dev.txt"
+  datasets=($1)
+else
+  DATA_LOADING_SCRIPT=mslm/ner_dataload.py
+  DATA_DIR=../WeLT/datasets/NER/
+  DEV_SET="devel.txt"
+  datasets=${ner_datasets}
+fi
+
+echo "Dataset loading script-" ${DATA_LOADING_SCRIPT}
+echo "Data dir-" ${DATA_DIR}
+
+meta_emb_sizes=(50 100 150 200)
 
 for DATASET_NAME in "${datasets[@]}"
 do
   CUDA_VISIBLE_DEVICES=0 \
   python3 run_train_no_tl.py \
     --model_name_or_path ${PRE_TRAINED_MODEL} \
-    --train_file ${DATA_DIR}/${DATASET_NAME}/train_dev.txt \
-    --validation_file ${DATA_DIR}/${DATASET_NAME}/devel.txt \
+    --train_file ${DATA_DIR}/${DATASET_NAME}/train.txt \
+    --validation_file ${DATA_DIR}/${DATASET_NAME}/${DEV_SET} \
     --test_file ${DATA_DIR}/${DATASET_NAME}/test.txt \
     --output_dir ${SAVE_DIR}/${DATASET_NAME} \
     --with_tracking \
@@ -28,7 +52,7 @@ do
     --task_name ner \
     --num_train_epochs ${EPOCHS} \
     --return_entity_level_metrics \
-    --loading_dataset_script mslm/ner_dataload.py \
+    --loading_dataset_script ${DATA_LOADING_SCRIPT} \
     --label_all_tokens \
     --per_device_train_batch_size ${BATCH_SIZE} \
     --per_device_eval_batch_size ${BATCH_SIZE} \
